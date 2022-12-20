bc6d46142d74b4c0ff5290e6b152c22f48748b44
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-12-20 02:54:14 -0800

    prototype standard

---
+++ Prototype-STANDARD/app/Http/Controllers/TaskController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+
+
+    public function __construct()
+    {
+            $this->middleware('auth')->except(['index']);
+            // OR
+            $this->middleware('auth')->only(['store','update','edit','create']);
+    }
+
+    function index(){
+
+        $task=Task::All();
+        return view("pages.index",compact('task'));
+    }
+    public function create(){
+
+        return view('pages.create');
+}
+    function store(Request $request){
+
+    $task = new Task();
+    $task->Task = $request->Task;
+    $task->save();
+    if( $task->save()){
+    return redirect('task');
+    }
+    }
+}
